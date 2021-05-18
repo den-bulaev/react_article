5404fac499a6b66ae39fd8ced3f6109c9a0848b6
Bulaev Denis <bulaevdenis7@gmail.com>
2021-05-18 13:20:11 +0300

    Updates

---
+++ precache-manifest.db9c81564089965204dfa41575324501.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0b6b92dc953324c9c9d4f131f2b2e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea7a00132c4903b459c7",
+    "url": "./static/js/2.cf384596.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cf384596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f712cce12bf217f83c",
+    "url": "./static/js/main.1194238d.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.db9c81564089965204dfa41575324501.js"
 );
 
 self.addEventListener('message', (event) => {
